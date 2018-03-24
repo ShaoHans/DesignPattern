d119ad250e07c36332ad2d4116384a4e9316b6d3
Hans <shaomenghao@hotmail.com>
2018-03-24 22:33:22 +0800

    原型模式

---
+++ 06.原型模式/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06.原型模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resume r1 = new Resume();
+            r1.SetWorkExperience("Tencent", 2011);
+            Resume r2 = (Resume)r1.Clone();
+            r2.SetWorkExperience("Alipay", 2016);
+
+            r1.Show();
+            r2.Show();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 06.原型模式/Resume.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.原型模式
+{
+    public class Resume : ICloneable
+    {
+        public string Company { get; protected set; }
+
+        public int Year { get; protected set; }
+
+        public void SetWorkExperience(string company,int year)
+        {
+            Company = company;
+            Year = year;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"{Year}年,我在{Company}公司上班");
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
